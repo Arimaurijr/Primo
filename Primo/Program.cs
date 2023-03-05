383751173d6da989de3fbb22181f547480ffb256
Ari Mauri <ari.mauri.jr@gmail.com>
2023-03-05 11:56:01 -0300

    Add project files.

---
+++ Primo/Program.cs
@@ -0,0 +1,37 @@
+﻿//primo
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double numero;
+
+        int count = 0;
+
+        Console.WriteLine("Digite um número");
+        numero = int.Parse(Console.ReadLine());
+
+        if (numero == 1)
+        {
+            Console.WriteLine("É primo");
+        }
+        else
+        {
+            for (int divisor = 1; divisor <= numero; divisor++)
+            {
+                if (numero % divisor == 0)
+                {
+                    count++;
+                }
+            }
+        }
+
+        if(count == 2)
+        {
+            Console.WriteLine("O número é PRIMO ");
+        }
+        else
+        {
+            Console.WriteLine("O número NÃO é PRIMO");
+        }
+    }
+}
